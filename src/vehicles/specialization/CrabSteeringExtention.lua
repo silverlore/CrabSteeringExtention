f94412e72b8fc27efb24ded06108a21f489f4850
Jimmy Nielsen <fragithub@silverdragons.dk>
2021-04-14 20:20:50 +0200

    fix xml modification when vehicle already have input bindings

--- src/vehicles/specialization/CrabSteeringExtention.lua
+++ src/vehicles/specialization/CrabSteeringExtention.lua
@@ -11,22 +11,43 @@ end
 function CrabSteeringExtention:onPreLoad(savegame)
 
     local i = 0
+    local keytemplate = "vehicle.crabSteering.steeringMode(%d)"
+    local needInputBinding = true
+    local steering = {}
+
     while true do
-        local key = ("vehicle.crabSteering.steeringMode(%d)"):format(i)
+        local key = (keytemplate):format(i)
         if not hasXMLProperty(self.xmlFile, key) then
             break
         end
-        local crabSteeringName = getXMLString(self.xmlFile, key .. "#name") 
+        
+        inputKey = key .. "#inputBindingName"
 
-        if(crabSteeringName == "action_steeringModeAllWheel") then
-            setXMLString(self.xmlFile, key .. "#inputBindingName", "CRABSTEERING_ALLWHEEL")
+        local input = getXMLString(self.xmlFile, inputKey)
+        if input ~= nil then
+            needInputBinding = false
+            break
         end
-        if(crabSteeringName == "action_steeringModeCrabLeft") then
-            setXMLString(self.xmlFile, key .. "#inputBindingName", "CRABSTEERING_CRABLEFT")
-        end
-        if(crabSteeringName == "action_steeringModeCrabRight") then
-            setXMLString(self.xmlFile, key .. "#inputBindingName", "CRABSTEERING_CRABRIGHT")
-        end
+        
+
+        local crabSteeringName = getXMLString(self.xmlFile, key .. "#name")
+        steering[crabSteeringName] = i
+
         i = i + 1
     end
+
+    if needInputBinding then 
+
+        if(steering["action_steeringModeAllWheel"] ~= nil) then
+            setXMLString(self.xmlFile, (keytemplate):format(steering["action_steeringModeAllWheel"]) .. "#inputBindingName", "CRABSTEERING_ALLWHEEL")
+        end
+        if(steering["action_steeringModeCrabLeft"] ~= nil) then
+            setXMLString(self.xmlFile, (keytemplate):format(steering["action_steeringModeCrabLeft"]) .. "#inputBindingName", "CRABSTEERING_CRABLEFT")
+        end
+        if(steering["action_steeringModeCrabRight"] ~= nil) then
+            setXMLString(self.xmlFile, (keytemplate):format(steering["action_steeringModeCrabRight"]) .. "#inputBindingName", "CRABSTEERING_CRABRIGHT")
+        end
+    end
+
+
 end
